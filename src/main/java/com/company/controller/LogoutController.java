cf56c1be47b080fbe761d96b73646c01c30fcfc5
mr_rashad <mr_rashad@email.com>
2023-11-11 13:47:50 +0400

    add

--- src/main/java/com/company/controller/LogoutController.java
+++ src/main/java/com/company/controller/LogoutController.java
@@ -1,14 +1,12 @@
 package com.company.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SignUpController {
-
-    @GetMapping("/sign-up.html")
-    public String signup() {
-        return "sign-up";
+public class LogoutController {
+    @GetMapping("login.html")
+    public String logout() {
+        return "login";
     }
 }
