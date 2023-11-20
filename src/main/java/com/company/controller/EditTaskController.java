4a362ce975361e62cddda080790de0c46accbf76
mr_rashad <mr_rashad@email.com>
2023-11-20 12:37:43 +0400

    add

---
+++ src/main/java/com/company/controller/EditTaskController.java
@@ -0,0 +1,54 @@
+package com.company.controller;
+
+import com.company.dto.request.TaskRequest;
+import com.company.dto.response.UserRespons;
+import com.company.service.impl.JwtServiceImpl;
+import com.company.service.impl.UserServiceImpl;
+import com.company.service.inter.TaskService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/addtask")
+public class EditTaskController {
+    private final TaskService taskService;
+    private final JwtServiceImpl jwtService;
+    private final UserServiceImpl userService;
+
+    @GetMapping()
+    public String task(HttpSession session, Model model) {
+        UserRespons userResp = (UserRespons) session.getAttribute("userRespons");
+        model.addAttribute("userRespons", userResp);
+        String token = (String) session.getAttribute("token");
+        String email = jwtService.extractUserName(token);
+        Long userId = userService.findByUserId(email);
+        model.addAttribute("userId", userId);
+        return "add-task";
+    }
+
+    @PostMapping
+    public String addTask(HttpSession session, TaskRequest request, Model model) {
+        String token = (String) session.getAttribute("token");
+        String email = jwtService.extractUserName(token);
+        Long userId = userService.findByUserId(email);
+        model.addAttribute("userId", userId);
+        taskService.createTaskForUser(userId, request);
+        return "redirect:/tasksdashboard";
+    }
+
+}
+//    @PostMapping
+//    public String addTask(HttpSession session, TaskRequest request, Model model) {
+//        String token = (String) session.getAttribute("token");
+//        model.addAttribute("token", token);
+//        String email = jwtService.extractUserName(token);
+//        Long userId = userService.findByUserId(email);
+//        taskService.createTaskForUser(userId, request);
+//        return "redirect:/tasksdashboard";
+//    }
