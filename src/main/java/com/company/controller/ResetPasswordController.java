76f8fd472449ec5d79febab3ddfe95823d46c3de
mr_rashad <mr_rashad@email.com>
2023-11-24 12:16:32 +0400

    add

--- src/main/java/com/company/controller/ResetPasswordController.java
+++ src/main/java/com/company/controller/ResetPasswordController.java
@@ -1,31 +1,31 @@
 package com.company.controller;
 
 import com.company.dto.request.UserRequest;
-import com.company.dto.response.UserRespons;
 import com.company.service.inter.NotificationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/resetpassword")
-public class RestPasswordController {
+public class ResetPasswordController {
     private final NotificationService notificationService;
 
-    @GetMapping
-    public String getResetPassword() {
+    @GetMapping("/reset")
+    public String getReset() {
         return "reset-password";
     }
+    @GetMapping("/resetpassword")
+    public String getResetPassword() {
+        return "sign-up";
+    }
 
-    @PostMapping
+    @PostMapping("/reset")
     public String postResetPassword(UserRequest userRequest) {
         String email = userRequest.getEmail();
         notificationService.sendNotificationPassword(email);
-        return "/login";
+        return "/resetpassword";
     }
 
 
--- src/main/java/com/company/controller/SignUpController.java
+++ src/main/java/com/company/controller/SignUpController.java
@@ -4,12 +4,9 @@ import com.company.dto.request.SignUpRequest;
 import com.company.dto.response.JwtAuthenticationResponse;
 import com.company.service.inter.AuthenticationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,7 +17,7 @@ public class SignUpController {
 
     @GetMapping()
     public String signup() {
-        return "sign-up";
+        return "reset";
     }
 
     private final AuthenticationService authenticationService;
@@ -30,7 +27,7 @@ public class SignUpController {
         JwtAuthenticationResponse response = authenticationService.signup(request);
         if (response == null) {
             System.out.println("register cannot be successfully");
-            return new ModelAndView("sign-up");
+            return new ModelAndView("reset");
         }
         return new ModelAndView("login");
     }
