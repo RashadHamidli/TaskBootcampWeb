36e1bd25cbd4bf7190c4f397ad261b1978c863ff
mr_rashad <mr_rashad@email.com>
2023-11-25 10:34:25 +0400

    add

--- src/main/java/com/company/controller/LogoutController.java
+++ src/main/java/com/company/controller/LogoutController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LogoutController {
     @GetMapping("logout")
     public String logout() {
-        SecurityContextHolder.clearContext();
-        return "redirect:/login";
+        return "redirect:/login?logout";
     }
 }
