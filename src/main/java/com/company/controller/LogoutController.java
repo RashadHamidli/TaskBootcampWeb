27eedbdf0424faeb0a35a7da0bdb5c64a1fba404
mr_rashad <mr_rashad@email.com>
2023-12-10 16:44:53 +0400

    add

--- src/main/java/com/company/controller/LogoutController.java
+++ src/main/java/com/company/controller/LogoutController.java
@@ -1,13 +1,14 @@
 package com.company.controller;
 
-import org.springframework.security.core.context.SecurityContextHolder;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LogoutController {
     @GetMapping("logout")
-    public String logout() {
-        return "redirect:/login?logout";
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login";
     }
 }
