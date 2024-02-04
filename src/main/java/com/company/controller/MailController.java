05b4dc35f9a238059af910679954e19abdb1b386
RashadHamidli <mr_rashad@email.com>
2024-02-04 22:38:44 +0400

    add

--- src/main/java/com/company/controller/MailController.java
+++ src/main/java/com/company/controller/MailController.java
@@ -4,9 +4,13 @@ import com.company.model.dao.entities.User;
 import com.company.service.inter.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MailController {
 
     private final NotificationService notificationService;
+    private final JavaMailSender mailSender;
 
     @GetMapping("/send")
     public String sendMail(Model model) {
@@ -30,5 +35,17 @@ public class MailController {
         }
 
         return "mail-result";
+    }
+    @PostMapping("/send-email")
+    public String sendEmail(@RequestParam("email") String email) {
+        // E-posta gönderme işlemi
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Şifre Sıfırlama Talebi");
+        message.setText("Merhaba, şifrenizi sıfırlamak için aşağıdaki linke tıklayın: <link>");
+        mailSender.send(message);
+
+        // Başarılı gönderim sonrası kullanıcıyı yönlendirme
+        return "redirect:/success.html";
     }
 }
